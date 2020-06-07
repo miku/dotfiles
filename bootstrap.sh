91d96d23e865812752f8b99bcf3e46ae37a97607
Martin Czygan <martin.czygan@gmail.com>
2020-06-07 13:51:24 +0200

    add makefile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-set -eu
-
 for req in rsync git; do
-    command -v $req >/dev/null 2>&1 || {
-        echo >&2 "$req required"
-        exit 1
-    }
+	command -v $req >/dev/null 2>&1 || {
+		echo >&2 "$req required"
+		exit 1
+	}
 done
 
 cd "$(dirname "${BASH_SOURCE}")"
@@ -14,14 +12,14 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
 function sync() {
-    rsync --exclude ".git/" \
-        --exclude ".DS_Store" \
-        --exclude ".osx" \
-        --exclude "bootstrap.sh" \
-        --exclude "README.md" \
-        --exclude "LICENSE" \
-        -avh --no-perms . ~
-    source ~/.bash_profile
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE" \
+		-avh --no-perms . ~
+	source ~/.bash_profile
 }
 
 sync
